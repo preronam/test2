8e8d2e35f7c2111139bb31bc9f5d1cfc21eabe54
Prerona Mukherjee <preronam@Preronas-MacBook-Pro.local>
2020-12-14 22:23:23 -0600

    final

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 let Product = require('../database/models/product.model')
 
-router.post('/product').post((req,res) => {
+router.post('/api/product/',(req,res) => {
 
  const username = req.body.username;
  const title = req.body.title;
@@ -9,16 +9,33 @@ router.post('/product').post((req,res) => {
  const category = req.body.category;
  const image = req.body.image;
 
- const newProduct = Product.create({
+ Product.create({
   username,
   title,
   description,
   category,
   image,
 
- });
+ }).then(dbusers =>{
+     res.json(dbusers);
+ })
+ .catch(err => {
+     res.json(err);
 
+ });
 });
 
 
+
+
 module.exports = router;
+
+// app.post("/submit", ({ body }, res) => {
+//     User.create(body)
+//       .then(dbUser => {
+//         res.json(dbUser);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
